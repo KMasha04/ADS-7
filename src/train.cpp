9be4686d8522048ae63656ec4423b3bafe2346df
KMasha04 <k.masha04030420@gmail.com>
2025-05-17 17:55:07 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -2,24 +2,24 @@
 #include "train.h"
 
 Train::Train() {
- first = nullptr;
- countOp = 0;
+first = nullptr;
+countOp = 0;
 }
 
 void Train::addCar(bool light) {
   Car* node = new Car;
   if (!first) {
-   node->light = light;
-   node->prev = node;
-   node->next = node;
-   first = node;
+  node->light = light;
+  node->prev = node;
+  node->next = node;
+  first = node;
   } else {
-   Car* SecFirst = first;
-   node->light = light;
-   node->next = first;
-   node->prev = first->prev;
-   first->prev->next = node;
-   first->prev = node;
+  Car *SecFirst = first;
+  node->light = light;
+  node->next = first;
+  node->prev = first->prev;
+  first->prev->next = node;
+  first->prev = node;
   }
 }
 
@@ -29,16 +29,16 @@ int Train::getLength() {
   int q = 1;
   int carriages = 1;
   while (first->light) {
-   carriages = 1;
-   SecFirst = first->next;
-   while (!SecFirst->light) {
+  carriages = 1;
+  SecFirst = first->next;
+  while (!SecFirst->light) {
     SecFirst = SecFirst->next;
     carriages += 1;
-   }
-   SecFirst->light = false;
-   countOp += carriages * 2;
   }
-   return carriages;
+  SecFirst->light = false;
+  countOp += carriages * 2;
+  }
+  return carriages;
 }
 int Train::getOpCount() {
   return countOp;
